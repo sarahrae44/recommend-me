e92929eb809e6f5fa9b576efbd4124db5fe7b8d6
Sarah Anderson <sarahanderson@MacBook-Pro.local>
2017-12-05 10:11:01 -0700

    index page up

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+app.get('/', (req, res) => {
+  res.render('index.ejs');
+});
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
