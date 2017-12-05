2530119f33c1f3b9eb3c1d17af25d8dc4accbebd
Sarah Anderson <sarahanderson@MacBook-Pro.local>
2017-12-05 10:14:54 -0700

    css file linked

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended:false}));
 
 app.get('/', (req, res) => {
   res.render('index.ejs');
 });
+
+app.use(express.static('public'));
 
 const port = process.env.PORT || 3000;
 
