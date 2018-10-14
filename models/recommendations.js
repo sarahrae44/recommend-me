35f5a681885472c5ac6790ebc35b0d236f91bacc
Sarah Anderson <sarahanderson@MacBook-Pro.local>
2018-10-14 11:25:23 -0600

    updated dependencies

--- models/recommendations.js
+++ models/recommendations.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const User = require('./users.js');
+// const User = require('./users.js');
 
 const recommendationSchema = new mongoose.Schema({
   title: String,
--- models/users.js
+++ models/users.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema({
  username: { type: String, unique: true },
  password: String,
  photo: String
- // recommendations: [Recommendations.schema]
+ recommendations: [Recommendations.schema]
 })
 
 const User = mongoose.model('User', userSchema);
